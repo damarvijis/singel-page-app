fcbefe22ea9379a2e3ed12fa84380c4f10b10de4
damarvijis <damar.j@mail.ugm.ac.id>
2023-07-30 18:38:44 +0700

    feat(webpack-plugin): add bundle for node js

--- webpack.base.js
+++ webpack.base.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: "./src/client/index.js",
   output: {
-    filename: "index_bundle.js",
+    filename: "index_client.js",
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, "public", "index.html") })],
