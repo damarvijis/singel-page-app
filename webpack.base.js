d42b56a0650e21b1ac00baedea7859bbfe7cbb67
damarvijis <damar.j@mail.ugm.ac.id>
2023-07-30 16:28:40 +0700

    feat(webpack-plugin): implement webpack plugin

--- webpack.base.js
+++ webpack.base.js
@@ -1,9 +1,11 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./src/client/index.js",
   output: {
-    filename: "index.js",
+    filename: "index_bundle.js",
     path: path.resolve(__dirname, "public"),
   },
+  plugins: [new HtmlWebpackPlugin()],
 };
