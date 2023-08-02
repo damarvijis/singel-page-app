3fa90c679d3bbf823f9ab043186895be34167052
damarvijis <damar.j@mail.ugm.ac.id>
2023-08-02 22:16:53 +0700

    feat(ts-loader) implement ts loader

--- webpack.base.js
+++ webpack.base.js
@@ -2,10 +2,22 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./dist-js/client/index.js",
+  entry: "./src/client/index.ts",
   output: {
     filename: "index_client.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
   },
   plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, "public", "index.html") })],
 };
